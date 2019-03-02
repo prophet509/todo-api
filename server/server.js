6abc2eec47e912e2c083dd8d2c76e0e35901aaf6
prophet509 <31301393+prophet509@users.noreply.github.com>
2019-03-02 15:34:24 +0700

    Add GET /todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const  express = require('express');
 const  bodyParser = require('body-parser');
 
-
+var {ObjectId} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
@@ -10,6 +10,7 @@ var {User} = require('./models/user');
 var app = express();
 
 app.use(bodyParser.json());
+
 app.post('/todos',(req,res) => {
     var todo = new Todo({
         text:req.body.text
@@ -20,6 +21,7 @@ app.post('/todos',(req,res) => {
         res.status(400).send(e);
       });
 });
+
 app.get('/todos', (req,res) => {
     Todo.find().then((todos) => {
         console.log(todos);
@@ -28,13 +30,30 @@ app.get('/todos', (req,res) => {
         res.status(400).send(e);
     });
 });
+app.get('/todos/:id',(req,res) => {
+    var id= req.params.id;
+    if(!ObjectId.isValid(id)){
+        return res.status(404).send();
+    }
+    Todo.findById(id).then((todo) => {
+        if(!todo){
+            return res.status(404).send();
+        }
+        res.send(todo);
+    },(e) => {
+        res.status(404).send();
+    });
+})
+
 app.get('/',(req,res) => {
     console.log("Da chay");
     res.send("aasdfasdf");
 });
+
 app.listen(3000,() => {
     console.log("Server is begin at",3000);
 });
+
 module.exports = {
     app
 };
